f34a30a29bdc6f2f78368b6df80ee2212a695dad
osidsouza <s.p.souza@hotmail.com>
2025-04-24 16:18:25 -0300

    feat(p1): exibe lista ficticia

---
+++ src/components/LocalidadeLista.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Card } from 'primereact/card'
+import { ficticios } from './ficticios'
+export default class LocalidadeLista extends Component {
+    render() {
+        const localidades = this.props.enderecos || ficticios
+        return (
+            <div className="grid p-4">
+                {localidades.map((item, dado) => (
+                    <div key={dado} className="col-12 mb-3">
+                        <Card
+                            title={item.logradouro}
+                            subTitle={`${item.bairro}, ${item.cidade} - ${item.uf}`}
+                            className="shadow-2" >
+                                <p className="m-0"><strong>CEP:</strong> {item.cep}</p>
+                        </Card>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+}
