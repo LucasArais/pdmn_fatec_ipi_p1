9fdd59299610d322705021a4d7eb3c681cb110c4
Lucas Arais <lucas_arais@hotmail.com>
2025-04-24 23:56:37 -0300

    feat(p1): integra com a api da viacep

--- src/components/Busca.jsx
+++ src/components/Busca.jsx
@@ -13,26 +13,36 @@ export default class Busca extends Component {
     }
 
     onTermoAlterado = (event) => {
-        console.log(event.target.value)
+        this.setState({ termoDeBusca: event.target.value })
+    }
+    
+
+ onFormSubmit = (event) => {
+        event.preventDefault()
+        this.props.onBuscaRealizada(this.state.termoDeBusca)
+
     }
 
-  render() {
+    render() {
     return (
-      <div
-        className='flex flex column'>
-            <IconField iconPosition='left'>
-                <InputIcon className='pi pi-search'></InputIcon>
-                <InputText 
-                className='w-full'
-                placeholder={this.props.dica}
-                onChange={this.onTermoAlterado}/>
-            </IconField>
-            <Button 
-                label = "OK" 
-                outlined/>
-    </div>
+        <form onSubmit={this.onFormSubmit}>
+            <div
+                className='flex flex-column'>
+                    <IconField iconPosition='left'>
+                        <InputIcon className='pi pi-search'></InputIcon>
+                        <InputText  
+                            className='w-full'
+                            placeholder={this.props.dica}
+                            onChange={this.onTermoAlterado}
+                            value={this.state.termoDeBusca}/>
+                    </IconField>
+                    <Button 
+                        label="OK"
+                        outlined/>
+            </div>
+        </form>
     )
-  }
+    }
 }
 
 Busca.defaultProps = {
--- src/components/LocalidadeLista.jsx
+++ src/components/LocalidadeLista.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Card } from 'primereact/card'
 import { ficticios } from './ficticios'
+
 export default class LocalidadeLista extends Component {
     render() {
         const localidades = this.props.enderecos || ficticios
